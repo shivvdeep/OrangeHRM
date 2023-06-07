00ee8c600d85b752e0d2fc9b7f7831145387c980
DELL <DELL@DESKTOP-L78V0UD>
2023-06-07 16:28:32 +0530

    test case2 was updated

--- Adrefresh/src/Testcases/TestCase2.java
+++ Adrefresh/src/Testcases/TestCase2.java
@@ -1,10 +1,13 @@
 package Testcases;
 
-public class TestCase2 {
+public class TestCase2 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		
+          System.out.println("this is test case 2");
+          System.out.println("test case 2 pubated");
 	}
 
 }
